bad16f6fb25c855c0e6b5dba8835f0d773bb646e
Ameer Azaz Arko <159276704+AmeerArko@users.noreply.github.com>
2026-07-08 11:53:16 +0600

    Add files via upload

---
+++ mutable.py
@@ -0,0 +1,8 @@
+spice_mix=set()
+print(f"Id of spice_mix: {id(spice_mix)}")
+spice_mix.add("Ginger")
+spice_mix.add("Cinnamon")
+
+
+
+print(f" id of after spices were added spice_mix after adding elements: {id(spice_mix)} ")
